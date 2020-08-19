393740bfc6f7a1f5d19eb58dbfe554d7cbd9a5e9
NGUYỄN HỬU TRÍ <35862674+1712832wine@users.noreply.github.com>
2020-08-19 08:48:21 +0700

    Ver 2.0.1

--- test.py
+++ test.py
@@ -1,28 +1,22 @@
 import csv
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 
 #Read Data
 ##############
-x= []
-y= []
-with open('X_train.csv','rt') as f:
-  data1 = csv.reader(f)
-  for i in data1:
-    x.append(i)
-with open('Y_train.csv','rt') as g:
-  data2 = csv.reader(g)
-  for i in data2:
-    y.append(i)
 
-a = []
-with open('X_test.csv','rt') as h:
-  data3 = csv.reader(h)
-  for i in data3:
-    a.append(i)
+def Read(fpath):
+  data = pd.read_csv(fpath) 
+  return data
 
+
+
+x= Read('X_train.csv')  #data train
+y= Read('Y_train.csv')  #label train
+a= Read('X_test.csv')   #data test
 
 #Pre Processing
 x=np.array(x)[1:,1:]                                         #Del First row and first col
